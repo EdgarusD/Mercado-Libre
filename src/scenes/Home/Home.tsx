a8204231e3e12f8c4e31f9701b0e72ae2c7771dc
Edgar Delgado <efrendelgado016@gmail.com>
2023-04-02 18:05:43 -0600

    config router

---
+++ src/scenes/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as M from '@mantine/core'
+
+export default function Home() {
+  return (
+    <M.Box>
+      <M.Text>
+        Home
+      </M.Text>
+    </M.Box>  
+  )
+}
